bf37e56605884951e9e5f846eac5a9982e513e58
jiri.turyna <jiri.turyna@jablotron.cz>
2022-03-23 00:32:49 +0100

    cv04

--- cv4/cv04.py
+++ cv4/cv04.py
@@ -10,38 +10,48 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import os
-import math
 
-def sort_histograms(data):
-    sum = 0
-    dir = os.path.abspath("cv03_v02")
-    files = list()
-    for file in os.listdir(dir):
-        file = os.path.join(dir, file)
-        files.append(file)
 
-    print(len(files))
-    #for file in files:
-        #print(file, data[file])
-     #   for i in range (0, 180):
-            #print(data[file][i][0])
-            #sum = sum + (hist1[i][0]-hist2[i][0])**2
-        #dist = math.sqrt(sum)
-        #print('euclidean distance:', dist)
+def plot_histograms(sortedData, files):
+    _, axs = plt.subplots(9, 9)
+    for i in range(len(sortedData)):
+        for j in range(len(sortedData)): 
+            image = cv2.cvtColor(cv2.imread(files[sortedData[i][j]]), cv2.COLOR_BGR2RGB)
+            axs[i][j].imshow(image)
+
+def sort_histograms(data, files):
+    sortedData = np.argsort(data)
+    sortedData = np.fliplr(sortedData)
+
+    sortedData[:, [len(sortedData)-1, 0]] = sortedData[:, [0, len(sortedData)-1]]
+    sortedData[:, [len(sortedData)-1, 2]] = sortedData[:, [2, len(sortedData)-1]]
+    print(sortedData)
+    return sortedData
     
 def load_histograms():
     result = dict()
+    files = list()
     dir = os.path.abspath("cv03_v02")
     for file in os.listdir(dir):
         file = os.path.join(dir, file)
-        print(file)
+        files.append(file)
         img = cv2.imread(file)
-        hue = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        hist = cv2.calcHist([hue], [0], None, [180], [0, 180])
-        print(hist)
+        hist = cv2.calcHist([img], [0], None, [255], [0, 255])
         result[file] = hist
+    
+    i = 0
+    array = [[0 for x in range(len(files))] for y in range(len(files))]
+    while(i < len(files)):
+        j = 0
+        while(j < len(files)):
+            if files[i] == files[j]:
+                pass
+            else:
+                array[i][j] = cv2.compareHist(result[files[i]], result[files[j]], cv2.HISTCMP_INTERSECT)
+            j += 1
+        i += 1
 
-    return result
+    return array, files
 
 
 def image_correction(file, etalon_file, c, index):
@@ -96,9 +106,14 @@ def image_equalization(file, index):
 
 
 if __name__ == '__main__':
-    #image_correction('cv04_f01.bmp', 'cv04_e01.bmp', 255, 1)
-    #image_correction('cv04_f02.bmp', 'cv04_e02.bmp', 255, 2)
-    #image_equalization('cv04_rentgen.bmp', 3)
-    #plt.show()
-    histograms = load_histograms()
-    sort_histograms(histograms)
+    image_correction('cv04_f01.bmp', 'cv04_e01.bmp', 255, 1)
+    image_correction('cv04_f02.bmp', 'cv04_e02.bmp', 255, 2)
+    image_equalization('cv04_rentgen.bmp', 3)
+    
+    data, files = load_histograms()
+    sortedData = sort_histograms(data, files)
+    plot_histograms(sortedData, files)
+    
+    plt.show()
+
+    
