2e14905991e73bdd6e1fdf43fefc050f31003564
jiri.turyna <jiri.turyna@jablotron.cz>
2022-04-18 17:40:05 +0200

    working

--- cv5/cv05.py
+++ cv5/cv05.py
@@ -20,25 +20,22 @@ def amplitudeSpectrum(data, files):
         i = 0
 
         img = cv2.imread(file, 0)
-        signalFFT = np.fft.fft2(img)
+        signalFFT = np.fft.ifft2(img)
         fshift = np.fft.fftshift(signalFFT)
         magnitude_spectrum = 20*np.log(np.abs(fshift))  
         img = cv2.imread(file)
 
         while i != len(data):
-            axs[i, 0].set_title('Median - před')
+            axs[i, 0].set_title('Před')
             axs[i, 0].imshow(img)
 
-            axs[i, 1].set_title('Median - po')
+            axs[i, 1].set_title('Po')
             axs[i, 1].imshow(data[i])
 
-            axs[i, 2].set_title('Median - spectrum')
+            axs[i, 2].set_title('Spectrum')
             axs[i, 2].imshow(magnitude_spectrum)
 
-            i = i+1 
-
-    plt.figure()
-        
+            i = i+1         
 
     #plt.imshow(magnitude_spectrum)
     return 0
@@ -59,7 +56,7 @@ def meanFilter(files):
     for file in files:
         img = cv2.imread(file)
         image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # convert to HSV
-        figure_size = 5 # the dimension of the x and y axis of the kernal.
+        figure_size = 5 # the dimension of the x and y axis of the kernel.
         newImage = cv2.blur(image,(figure_size, figure_size))
         newImage = cv2.cvtColor(newImage, cv2.COLOR_HSV2BGR) # convert to HSV
 
@@ -68,7 +65,18 @@ def meanFilter(files):
     return means
 
 def rotatingMask(files):
-    return 0
+    means = list()
+
+    for file in files:
+        img = cv2.imread(file)
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # convert to HSV
+        figure_size = 5 # the dimension of the x and y axis of the kernel.
+        newImage = cv2.blur(image,(figure_size, figure_size))
+        newImage = cv2.cvtColor(newImage, cv2.COLOR_HSV2BGR) # convert to HSV
+
+        means.append(newImage)
+        
+    return means
 
     
 def loadFiles(folder):
@@ -86,10 +94,10 @@ if __name__ == '__main__':
 
     medians = medianFilter(files)
     means = meanFilter(files)
-    #masks = rotatingMask(files)
+    masks = rotatingMask(files)
     data.extend(medians)
     data.extend(means)
-    #data.extend(masks)
+    data.extend(masks)
     print(len(data))
 
     amplitudeSpectrum(data, files)
